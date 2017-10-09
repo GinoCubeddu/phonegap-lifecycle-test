60a2a988f926ed04eda9d9e70e84e4f9ba7b76d5
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2017-10-09 09:50:54 +0100

    Removed jquery-mobile

--- www/js/index.js
+++ www/js/index.js
@@ -2,9 +2,10 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+// EVENT LSITENER FOR DEVICE READY IS HERE
 document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
+
+
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
@@ -13,32 +14,33 @@ function updateDisplay() {
 
 
 // device APIs are available
-//
-    function onDeviceReady() {
-	
+// DEVICE READY HANDLER IS HERE
+function onDeviceReady() {
+
+	// EVENT LISTENER FOR WHEN APP IS RESUMED
 	document.addEventListener("resume", onResume, false);
+	// EVENT LISTENER FOR WHEN APP IS PAUSED
 	document.addEventListener("pause", onPause, false);
-	
+
 	launched_count++;
 	updateDisplay();
-	    
+
 	alert("device ready");
-    }
+}
 
 
-    function onPause() {
-	
+// HANDLER FOR WHEN APP IS PAUSED
+function onPause() {
 	paused_count++;
 	updateDisplay();
-	    
-	alert("pause");
-    }
-	
 
-    function onResume() {
-		
+	alert("pause");
+}
+
+// HANDLER FOR WHEN APP IS RESUMED
+function onResume() {
 	resumed_count++;
 	updateDisplay();
-	    
+
 	alert("resume");
-    }
+}
